c519e0979ccbdc8918fa91e5fe7f48c6e974032f
emidio <emidio@DESKTOP-SS2BVK5>
2019-06-23 20:33:13 -0300

    Mais alterações na classe aluno, Tentativa para armazenar e imprimir as avaliacoes diarias

--- QAcademico/src/entidades/Aluno.java
+++ QAcademico/src/entidades/Aluno.java
@@ -1,64 +1,44 @@
 package entidades;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Aluno extends Pessoa
 {
-//	Date diaLetivo = null;
-//	NotaDeProva notaAluno;
-//	
-//	Map<Date, Integer> historicoDeAvaliacoes;
-//	
+	Date diaLetivo = null;
+	NotaDeProva notaAluno;
+	
+	Calendar calendario = Calendar.getInstance();
+	int dia = calendario.get(Calendar.DAY_OF_MONTH);
+	
+	Map<Integer, Integer> historicoDeAvaliacoes;
+	
 //	public Aluno()
 //	{
 //		historicoDeAvaliacoes = new HashMap<Date, Integer>();
 //	}
+
+	public Aluno()
+	{
+		historicoDeAvaliacoes = new HashMap<Integer, Integer>();
+	}
+
 	
-//	ArrayList<Pessoa> listaAvaliacaoDiaria = new ArrayList<>();
-	int[] listaAvaliacaoDiaria = new int[30];{
-	
-	listaAvaliacaoDiaria[0] = 2;
-	listaAvaliacaoDiaria[1] = 2;
-	listaAvaliacaoDiaria[2] = 2;
-	listaAvaliacaoDiaria[3] = 1;
-	listaAvaliacaoDiaria[4] = 2;
-	listaAvaliacaoDiaria[5] = 2;
-	listaAvaliacaoDiaria[6] = 4;
-	listaAvaliacaoDiaria[7] = 2;
-	listaAvaliacaoDiaria[8] = 2;
-	listaAvaliacaoDiaria[9] = 4;
-	listaAvaliacaoDiaria[10] = 2;
-	listaAvaliacaoDiaria[11] = 2;
-	listaAvaliacaoDiaria[12] = 3;
-	listaAvaliacaoDiaria[13] = 2;
-	listaAvaliacaoDiaria[14] = 3;
-	listaAvaliacaoDiaria[15] = 2;
-	listaAvaliacaoDiaria[16] = 4;
-	listaAvaliacaoDiaria[17] = 2;
-	listaAvaliacaoDiaria[18] = 2;
-	listaAvaliacaoDiaria[19] = 1;
-	listaAvaliacaoDiaria[20] = 2;
-	listaAvaliacaoDiaria[21] = 1;
-	listaAvaliacaoDiaria[22] = 2;
-	listaAvaliacaoDiaria[23] = 2;
-	listaAvaliacaoDiaria[24] = 2;
-	listaAvaliacaoDiaria[25] = 2;
-	listaAvaliacaoDiaria[26] = 2;
-	listaAvaliacaoDiaria[27] = 2;
-	listaAvaliacaoDiaria[28] = 2;
-	listaAvaliacaoDiaria[29] = 2;
+	public Map<Integer, Integer> avaliarDia(int dia, int nota) {
+		historicoDeAvaliacoes.put(dia, nota);
+		return historicoDeAvaliacoes;
 	}
 	
-	public int[] exibirAvaliacaoDiaria() {
-			this.listaAvaliacaoDiaria;
-		for (int i = 0; i < listaAvaliacaoDiaria.length; ++i) {
-			System.out.println(listaAvaliacaoDiaria[i]);
-			
+	public void mostrarRelatorio() {
+		for (int i = 0; i < historicoDeAvaliacoes.size(); ++i) {
+			int dia = historicoDeAvaliacoes.hashCode();
+			System.out.println(historicoDeAvaliacoes.get(dia));	
 		}
 	}
+	
 		
 
 }
--- QAcademico/src/inicio/Inicio.java
+++ QAcademico/src/inicio/Inicio.java
@@ -17,21 +17,26 @@ public class Inicio
 		Aluno alexsandro = new Aluno();
 		alexsandro.setMatricula("20161y6-RC0050");
 		alexsandro.setNome("Alexsandro Matias de Almeida");
+		alexsandro.avaliarDia(0, 3);
+		alexsandro.avaliarDia(1, 3);
+		alexsandro.avaliarDia(2, 3);
+		alexsandro.avaliarDia(3, 3);
+		alexsandro.avaliarDia(4, 3);
 		
 		erivelton.setMatricula("20162y6-RC0040");
 		erivelton.setNome("Erivelton Ribeiro da Silva Alves");
+		erivelton.avaliarDia(0, 3);
+		erivelton.avaliarDia(1, 3);
+		erivelton.avaliarDia(2, 3);
+		erivelton.avaliarDia(3, 3);
+		erivelton.avaliarDia(4, 3);
 		
 		Professor paulo = new Professor();
 		paulo.setNome("Paulo Abadie Guedes");
 		paulo.setMatricula("20003y6-PROF001");
 		
-//		System.out.println(alexsandro.getNome());
-//		paulo.avaliarDiariamenteAluno(alexsandro.getMatricula(), Avaliacao.bom);
 		
-//		System.out.println(erivelton.getNome());
-//		paulo.avaliarDiariamenteAluno(alexsandro.getMatricula(), Avaliacao.regular);
-		
-		ArrayList<Pessoa> listaAlunos = new ArrayList<>();
+		ArrayList<Aluno> listaAlunos = new ArrayList<>();
 		
 		listaAlunos.add(alexsandro);
 		listaAlunos.add(erivelton);
@@ -39,8 +44,10 @@ public class Inicio
 		for (int i = 0; i < listaAlunos.size(); ++i) {
 			Pessoa alunosTurma = listaAlunos.get(i);
 			System.out.println(alunosTurma.getNome());
-			
 		}
+		
+		alexsandro.mostrarRelatorio();
+		erivelton.mostrarRelatorio();
 		
 	}
 }
